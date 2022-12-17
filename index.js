b700af23c5e1651a2e35524ec97735fe9e6c46f1
Prabhatlodhi <lodhi.prabhat@gmail.com>
2022-12-17 15:29:06 +0530

    qww

--- index.js
+++ index.js
@@ -18,8 +18,7 @@ let weather = {
     const { speed } = data.wind;
 
     document.querySelector(".city").innerText = "w in" + name;
-    document.querySelector(".icon").src =
-      "https://openweathermap.org/img/wn/" + icon + ".png";
+    document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + ".png";
     document.querySelector(".description").innerText = description;
     document.querySelector(".temp").innerText = temp + "C";
     document.querySelector(".humidity").innerText = "Humidity" + humidity + "%";
@@ -31,13 +30,16 @@ let weather = {
   },
 };
 
+
+
+
+
+
 document.querySelector(".search button").addEventListener("click", function () {
   weather.search();
 });
 
-document
-  .querySelector(".searchBar")
-  .addEventListener("keyup", function (event) {
+document.querySelector(".searchBar").addEventListener("keyup", function (event) {
     if (event.key == "Enter") {
       weather.search();
     }
